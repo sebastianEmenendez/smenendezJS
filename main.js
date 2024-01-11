f7bba3da2faa9752870c6852d8722b60d037fda3
sebastianmenendez <sebastianmnndz81@gmail.com>
2024-01-11 19:38:06 -0300

    variable if

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-let a = 2
-let b = 5
-let resultado = a * b
 
-document.write(resultado)
+const bandas = ['Black Sabbath', 'Led Zepellin', 'Deep Purple', 'Judas Priest', 'Iron Maiden', 'Metallica', 'Megadeth',
+    'Slayer', 'Pantera', 'V8', 'Hermetica', 'Almafuerte', 'Horcas', 'Dream Theater', 'Death', 'Amon Amarth', 'Sepultura',
+    'Stratovarius', 'Dragon Force', 'Symphony X', 'Helloween', 'Dark Tranquility'];
+
+
+let nombre = prompt("¿Cuál es tu nombre?");
+let tuBanda = prompt("Ingresa tu banda");
