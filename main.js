96827348b533cdde73d8ace5a1ac496c8373233f
sebastianmenendez <sebastianmnndz81@gmail.com>
2024-01-11 19:41:34 -0300

    html y css

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const bandas = ['Black Sabbath', 'Led Zepellin', 'Deep Purple', 'Judas Priest', 
 
 let nombre = prompt("¿Cuál es tu nombre?");
 let tuBanda = prompt("Ingresa tu banda");
+
+if (bandas.includes(tuBanda)) {
+        console.log("¡Bienvenido hermano metalero , " + nombre + " ! Tu banda es de las grandes del metal.");
+    } else {
+        // Si la banda no está en el array, agrégala
+        bandas.push(tuBanda);
+        console.log("¡Bienvenido hermano metalero , " + nombre + "! Tu banda favorita ha sido agregada a nuestra lista metalera.");
+    }
